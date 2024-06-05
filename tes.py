daf30487ba723b8d3160013902b5b4cd0c2704bd
mellowraiin <nicosantoso02@gmail.com>
2024-06-05 23:43:09 +0700

    another testing

--- tes.py
+++ tes.py
@@ -2,9 +2,9 @@
 def x(var1):
     def y(var2):
         return var2+1
-    z = y(3)
-    return z+var1
+    a = y(3)
+    def z(var3):
+        return var3
+    b=z(2)
+    return a+var1+b
 
-
-
-print(x(1))
